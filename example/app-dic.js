cc0ae4a1abb3db7f849f44b9564d13bbedcb9cb5
Aleksandr Gornostal <sanya.gornostal@gmail.com>
2013-12-23 19:06:34 +0200

    few fixes

--- example/app-dic.js
+++ example/app-dic.js
@@ -1,16 +1,16 @@
-define(['require'], function(require) {
+define(function(require) {
 	var Dic = require('../dic');
 	var AQ = require('AQ');
 
 	return new Dic({
-		'controllers/player': function(config){
-			var ControllerPlayer = require('controllers/player'),
+		'controllers/player': function(options){
+			var ControllerPlayer = require('appname/controllers/player'),
 				player = this.getSingleton('models/player');
-			return new ControllerPlayer(AQ, player, config.element);
+			return new ControllerPlayer(AQ, player, options.element);
 		},
 
 		'models/player': function(){
-			var Player = require('models/player');
+			var Player = require('appname/models/player');
 			return new Player(AQ);
 		}
 	});
